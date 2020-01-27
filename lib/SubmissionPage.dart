d4181c16bc4514cdb2e0b1cf1a57feb0378c6d98
Brian Johncox <brianjohncox232@gmail.com>
2020-01-26 21:46:29 -0500

    Initial Commit

---
+++ lib/SubmissionPage.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+
+class SubmissionPage extends StatelessWidget {
+  const SubmissionPage({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text("Welcome to PurdueHCR"),
+    );
+  }
+}
